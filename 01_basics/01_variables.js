73f06c8f4568ac94f97316cebb5933f16996d605
Ankit Agrawal <152623790+agra-ankit@users.noreply.github.com>
2026-06-27 17:03:00 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "naruto@gmail.com"
+var accountPassword = "12345"          
+accountCity = "Jaipur"
+let accountState;
+// accountId =2
+accountEmail = "hc@hc.com"
+accountPassword= "21212122"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
