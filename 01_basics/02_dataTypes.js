549445a53294f30b3486be8a8d4da9df07208e3d
Ankit Agrawal <152623790+agra-ankit@users.noreply.github.com>
2026-06-27 17:03:30 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,17 @@
 "use strict";
 // alert(3 + 3) //we are using nodejs , not browser
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
 
+let state = null  
+// number => 2 to the power 53 (a definite range)
+// bigint
+// string  => ""
+// boolean => true/false
+// null =>standalong value
+// undefined =>
+// symbol =>unique
+
+console.log(typeof undefined); //undefined
+console.log(typeof null) ; //object
